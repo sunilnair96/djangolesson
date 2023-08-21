86f348a6e8ea7c782ef10dd48856de1b656db367
sunilnair96 <sunilnair96@yahoo.com>
2023-08-22 07:22:02 +1000

    lecture03

---
+++ lecture3/hello/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, "hello/index.html")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name" : name.capitalize()
+    })
---
+++ lecture3/newmonth/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NewmonthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'newmonth'
